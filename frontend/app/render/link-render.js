ed2d60a8562b41591e2e14b1e7e50ab1c546d715
Dmitry Pavlov <dpavlov@wiley.com>
2017-03-08 02:12:09 +0300

    Update link rendering

--- frontend/app/render/link-render.js
+++ frontend/app/render/link-render.js
@@ -31,7 +31,7 @@ export default class LinkRender {
   }
   line(x1, y1, x2, y2) {
       this.ctx.beginPath();
-      this.ctx.lineWidth = 1;
+      this.ctx.lineWidth = 2;
       this.ctx.strokeStyle = '#000000';
       this.ctx.moveTo(x1, y1);
       this.ctx.lineTo(x2, y2);
