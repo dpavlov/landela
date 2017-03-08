aaacde7d0c28ca1d7f6e1648c4bf7e111b71bc8f
dpavlov <dm.pavlov@inbox.ru>
2017-03-08 03:28:53 +0300

    Update site render to display name

--- frontend/app/render/site-render.js
+++ frontend/app/render/site-render.js
@@ -1,4 +1,5 @@
 import Offset from '../geometry/offset';
+import Point from '../geometry/point';
 
 export default class SiteRender {
   constructor(settings, viewport, ctx, nodeRender) {
@@ -12,12 +13,18 @@ export default class SiteRender {
     let w = this.viewport.withScale(site.width);
     let h = this.viewport.withScale(site.height);
     this.roundRect(siteCenter.x - w / 2, siteCenter.y - h / 2, w, h, this.viewport.withScale(20));
-
+    this.text(site.name, new Point(siteCenter.x - w / 2 + this.viewport.withScale(20), siteCenter.y + h / 2 - this.viewport.withScale(10)));
     for (var iNode = 0; iNode < site.nodes.length; iNode++) {
         var node = site.nodes[iNode];
         this.nodeRender.render(node);
     }
 
+  }
+  text(t, s) {
+    this.ctx.font= this.viewport.withScale(16) + "px Times";
+    this.ctx.fillStyle = "#c0e2f7";
+    this.ctx.textAlign="left";
+    this.ctx.fillText(t, s.x, s.y);
   }
   roundRect(x, y, width, height, r) {
     let radius = {tl: r, tr: r, br: r, bl: r};
